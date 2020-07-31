c3b41484aa08cde609deacfe3e7dbba1d17d9fc6
devisupriyadi <devisupriyadi0310@gmail.com>
2020-07-31 19:09:41 +0700

    loading...

--- app/src/main/java/com/devisupriyadi/jtcapps/KontakFragment.kt
+++ app/src/main/java/com/devisupriyadi/jtcapps/KontakFragment.kt
@@ -1,10 +1,13 @@
 package com.devisupriyadi.jtcapps
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_kontak.*
 
 class KontakFragment : Fragment() {
 
@@ -14,6 +17,18 @@ class KontakFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_kontak, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        ig1.setOnClickListener(){
+            var i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/jtc_team/"))
+            startActivity(i)
+        }
+        maps1.setOnClickListener(){
+            var i = Intent(Intent.ACTION_VIEW, Uri.parse("https://goo.gl/maps/xBzJEVzaGWB2vbbw5"))
+            startActivity(i)
+        }
     }
 
     companion object {
